9b50626f64b796656745810c5ce3e9ed5f47bae5
Karuppiah <karuppiah7890@gmail.com>
2015-11-25 13:22:56 +0530

    Add loginform.inc.php for form fields

---
+++ loginform.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+
+?>
+
+<form action="<?php echo $current_file ?>" method="POST">
+
+	Username : <input type="text" name="username"/><br/>
+	Password : <input type="password" name="password"/><br/>
+	<input type="submit" value="Log in"/><br/>
+
+</form>
