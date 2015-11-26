d12751726e155821fbe0fa80f3d166b529b437a2
Karuppiah <karuppiah7890@gmail.com>
2015-11-26 22:48:54 +0530

    Add getUserFieldValue() function to get user fields from Table Table : login

--- auth_core.inc.php
+++ auth_core.inc.php
@@ -2,17 +2,31 @@
 
 	session_start();
 
-	if (isset($_SESSION['username'])&&!empty($_SESSION['username']))
-	$username = $_SESSION['username'];
-
 	$current_file = $_SERVER['SCRIPT_NAME'];
 	
 	function loggedin()
 	{
-		if (isset($_SESSION['user_id'])&&!empty($_SESSION['user_id'])) {
+		if (!empty($_SESSION['user_id'])) {
 			return true;
 		}
 		else
 			return false;
 	}
+
+	function getUserFieldValue($fieldname)
+	{
+		global $conn;
+
+		$query = "SELECT `$fieldname` FROM login WHERE `id`=".$_SESSION['user_id']."";
+
+		if($query_run = mysqli_query($conn,$query)){
+
+			$row = mysqli_fetch_assoc($query_run);
+
+			if(!empty($row[$fieldname]))
+				return $row[$fieldname];
+		}
+
+	}
+
 ?>
